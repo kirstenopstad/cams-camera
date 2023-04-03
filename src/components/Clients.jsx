20461ceb9e660efed434baaef3831fe6434390bf
robbryandev <robbryandev@gmail.com>
2023-04-03 14:52:49 -0700

    Make Clients responsive

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,15 +1,19 @@
-import clients from "@/data/clients"
+import clients from "@/data/clients";
+import { Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import {v4} from "uuid";
 
 export default function Clients() {
     return (
-        <div>
+        <div className="space-y-1 m-2">
             {
                 clients.map((client) => {
                     return (
-                        <div key={v4()}>
-                            <p>{client.name}</p>
-                            <img src={client.logo} alt={`${client.name}'s Logo`} />
+                        <div className="card w-1/2 md:w-1/4 2 max-w-[250px] bg-light" key={v4()}>
+                            <div className="card-body relative p-2">
+                                <img className="object-fill w-1/3 inline mb-1 mr-2 aspect-square" src={client.logo} alt={`${client.name}'s Logo`}/>
+                                <p className="text-center inline ml-4 pl-6 mt-4 border-s border-black font-semibold">{client.name}</p>
+                            </div>
                         </div>
                     )
                 })
--- src/data/clients.js
+++ src/data/clients.js
@@ -3,6 +3,14 @@ const clients = [
     name: "test",
     logo: "favicon.ico",
   },
+  {
+    name: "Very very very very long brand name with way too many words lol",
+    logo: "favicon.ico",
+  },
+  {
+    name: "Very large image test",
+    logo: "img/camera-closeup.jpg",
+  },
 ];
 
 export default clients;
