c121676f7de35edf45510664bf1fce48a0c2e0f1
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-04-03 16:23:54 -0700

    add cond. rendering for rental list/details

--- src/components/RentalsControl/RentalDetails.jsx
+++ src/components/RentalsControl/RentalDetails.jsx
@@ -1,5 +1,25 @@
-export default function RentalDetails() {
+import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+
+
+
+export default function RentalDetails({ item, onCloseClick }) {
     return (
-        <></>
+        <>
+        <h2>{item.brand} {item.model}</h2>
+        {item.photos.map((photo, index) => 
+            <img 
+            key={`${item}${index}`} 
+            style={{width:'300px'}}
+            src={photo}/>
+        )}
+        <p>{item.description}</p>
+        <Button variant='outline-dark'>Add to Quote</Button>
+        <Button variant='outline-dark' onClick={onCloseClick}>Close</Button>
+        </>
     )
+}
+
+RentalDetails.propTypes = {
+    onCloseClick: PropTypes.func
 }
--- src/components/RentalsControl/RentalList.jsx
+++ src/components/RentalsControl/RentalList.jsx
@@ -1,31 +1,49 @@
 import inventory from './../../RentalInventory'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import PropTypes from 'prop-types';
 import Carousel from 'react-bootstrap/Carousel';
+import styles from '@/styles/Rentals.module.css';
 
-import styles from '@/styles/Rentals.module.css'
-
-export default function RentalList() {
+export default function RentalList({ onItemClick }) {
     return (
         <>
-        {inventory.map((item) => 
-            <Card key={item.id} style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={item.photos[0]}/>
-                <Card.Body>
-                <Carousel.Item>
-                {item.photos.map((photo, index) => 
-                    <Card.Img variant="top" key={`${item}${index}`} src={photo}/>
-                )}
-                </Carousel.Item>
-                <Card.Title>{item.brand} {item.model}</Card.Title>
-                <Card.Text>
-                    {item.description}
-                </Card.Text>
-                <Button variant="outline-dark">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-            
-            )}
+        <Container>
+            <Row xs={1} md={2} className='g-4'>
+                <Col>
+                    {inventory.map((item) => 
+                        <Card key={item.id} xs={1} lg={2} style={{ width: '25rem' }}>
+                            {/* <Carousel>
+                                <Carousel.Item>
+                                {item.photos.map((photo, index) => 
+                                    <img key={`${item}${index}`} className="d-block w-100" src={photo}/>
+                                    )}
+                                </Carousel.Item>
+                            </Carousel> */}
+                            <Card.Img 
+                                variant="top" 
+                                src={item.photos[0]}
+                                onClick={() => onItemClick(item)}/>
+                            <Card.Body>
+                            
+                            <Card.Title>{item.brand} {item.model}</Card.Title>
+                            <Card.Text>
+                                {item.description}
+                            </Card.Text>
+                            <Button variant="outline-dark">Add to Quote</Button>
+                            </Card.Body>
+                        </Card>
+                        )}
+                </Col>
+            </Row>
+        </Container>
         </>
     )
+}
+
+RentalList.propTypes = {
+    onItemClick: PropTypes.func
 }
