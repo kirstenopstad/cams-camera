42fc5b7b58ae12d9944266907b2291761fef9d04
robbryandev <robbryandev@gmail.com>
2023-04-04 13:12:18 -0700

    Group same items together in cart

--- src/components/RentalsControl/Cart.jsx
+++ src/components/RentalsControl/Cart.jsx
@@ -1,12 +1,40 @@
-import PropTypes from 'prop-types';
+/* eslint-disable react-hooks/exhaustive-deps */
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
-export default function Cart({items}) {
+export default function Cart({ items }) {
+  const [newItems, setNewItems] = useState([]);
+  const [count, setCount] = useState({});
+
+  // filter 1 of each item
+  useEffect(() => {
+    let checkItems = [];
+    let itemCount = {};
+    setNewItems(
+      items.filter((i) => {
+        const iName = i.model.replace(/ /g, "_");
+        if (checkItems.includes(i)) {
+          itemCount[iName] += 1;
+          return false;
+        } else {
+          checkItems.push(i);
+          itemCount[iName] = 1;
+          return true;
+        }
+      })
+    );
+    setCount(itemCount);
+  }, [items]);
+
   return (
     <div className="my-2">
-      {items.map((item) => {
+      {newItems.map((item) => {
+        const itemName = item.model.replace(/ /g, "_");
         return (
           <>
-            <p>{item.brand} {item.model}</p>
+            <p>
+              {item.brand} {item.model} {count[itemName] > 1 ? `* ${count[itemName]}` : null}
+            </p>
           </>
         );
       })}
@@ -15,5 +43,5 @@ export default function Cart({items}) {
 }
 
 Cart.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.object)
-}
+  items: PropTypes.arrayOf(PropTypes.object),
+};
