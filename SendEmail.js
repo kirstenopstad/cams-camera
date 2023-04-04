801c4cc2ff634f5e74068eaca47418e1921a7183
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-04-04 13:57:43 -0700

    BUGFIX: next env var exposure + tempParam values

--- SendEmail.js
+++ SendEmail.js
@@ -1,23 +1,25 @@
-import emailjs from '@emailjs/browser';
+import emailjs from '@emailjs/browser'
 
 // these populate email templates, edit with caution
 const COMPANY = "Cam's Camera"
 const CC_LIST = "kirsten.opstad@gmail.com, builtbyko@gmail.com"
 
 // takes formData as input and returns success or error message
-export default sendEmail = ({formData}) => {
+const sendEmail = (formData) => {
+  console.log(formData)
   // NOTE: ensure formData.emailType is included
   let templateParams = {};
+  let templateId = null;
   // if intro email, send to 
   if (formData.emailType === "intro") {
     templateParams = {
       // parameters match existing template -> if changed, emailJS template must be changed
       user_name: formData.userName,
       user_email: formData.userEmail,
-      message: formData.messsage
+      message: formData.message
     };
     // use intro template (sent to company)
-    templateId = process.env.REACT_APP_EMAILJS_INTRO_TEMPLATE_ID;
+    templateId = process.env.NEXT_PUBLIC_EMAILJS_INTRO_TEMPLATE_ID;
   } else if (emailType === "quote") {
     // TODO: build message for quote
     templateParams = {
@@ -29,10 +31,9 @@ export default sendEmail = ({formData}) => {
       ccList: CC_LIST
     };
     // use intro template (sent to client, cc'd to ccList)
-    templateId = process.env.REACT_APP_EMAILJS_QUOTE_TEMPLATE_ID;
+    templateId = process.env.NEXT_PUBLIC_EMAILJS_QUOTE_TEMPLATE_ID;
   }
-
-  emailjs.send(process.env.REACT_APP_EMAILJS_SERVICE_ID, templateId, templateParams, process.env.REACT_APP_EMAILJS_PUBLIC_KEY)
+  emailjs.send(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, templateId, templateParams, process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY)
   .then((result) => {
       // return success message
       if (result.status === 200) {
@@ -44,3 +45,5 @@ export default sendEmail = ({formData}) => {
       return (`${error.status} - ${error.text}`)
   });
 };
+
+export default sendEmail;
