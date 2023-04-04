e3f6a8decfac821c574b670d61b757245f549008
robbryandev <robbryandev@gmail.com>
2023-04-04 10:50:37 -0700

    Center clients and change sizing

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -4,12 +4,12 @@ import {v4} from "uuid";
 
 export default function Clients() {
     return (
-        <div className="flex flex-row flex-wrap gap-10 m-4">
+        <div className="flex flex-row flex-wrap justify-center gap-10 m-4">
             {
                 clients.map((client) => {
                     return (
-                        <div className="w-20 h-20" key={v4()}>
-                            <div className="w-full h-full mb-2 bg-black rounded-full">
+                        <div className="w-20 h-auto" key={v4()}>
+                            <div className="w-20 h-20 mb-2 bg-black rounded-full">
                                 <img className="object-cover w-full h-full rounded-full" src={client.logo} alt={`${client.name}'s Logo`} />
                             </div>
                             <p className="pt-2 font-semibold text-center text-md">{client.name}</p>
