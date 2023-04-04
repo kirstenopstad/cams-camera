377f005f59df9196f6d6b12ca69404f54a69d91e
robbryandev <robbryandev@gmail.com>
2023-04-04 14:08:37 -0700

    Add comments to cart utility

--- src/utils/cart.js
+++ src/utils/cart.js
@@ -1,13 +1,18 @@
 export function filterCart(cartItems) {
   let check = [];
   let result = { count: {}, items: [] };
+  // filter the array of items
   result.items = cartItems.filter((i) => {
     const iName = i.model.replace(/ /g, "_");
+    // check if the item is in the check array
     if (check.includes(i)) {
+      // if it is, increment the count for that item
       result.count[iName] += 1;
       return false;
     } else {
+      // if it is not, add it to the check array
       check.push(i);
+      // set the count for that item to 1
       result.count[iName] = 1;
       return true;
     }
