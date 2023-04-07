bed79c5f8630b337cf69f18da0fa36fd88c5f438
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-04-07 11:48:17 -0700

    add handleDecrement, handleRemove

--- src/components/RentalsControl.jsx
+++ src/components/RentalsControl.jsx
@@ -11,6 +11,26 @@ export default function RentalsControl() {
     setCart([...cart, item]);
   };
 
+  const handleDecrementItemInCart = (item) => {
+    // get first instance of this type of item
+    const itemToRemove = cart.filter(i => i.id === item.id)[0]
+    // get index of itemToRemove
+    const indexToRemove = cart.indexOf(itemToRemove)
+    // make copy of cart
+    const updatedCart = [...cart]
+    // splice it out of the cart
+    updatedCart.splice(indexToRemove, 1)
+    // update cart
+    setCart(updatedCart)
+  }
+  
+  const handleRemoveFromCart = (item) => {
+    // make copy of cart that removes all instances of item
+    const updatedCart = cart.filter(i => i.id != item.id)
+    // update cart
+    setCart(updatedCart)
+  }
+
   return (
     <div id="rental-control">
       <h1>Rentals</h1>
