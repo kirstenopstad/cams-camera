43097572524ef23502f361b05fe371016d13ae56
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-04-07 11:20:34 -0700

    add inc, dec, remove UI

--- src/components/RentalsControl/Cart.jsx
+++ src/components/RentalsControl/Cart.jsx
@@ -2,6 +2,10 @@
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { filterCart } from "@/utils/cart";
+import styles from "@/styles/Cart.module.css"
+import Table from "react-bootstrap/Table";
+
+
 
 export default function Cart({ items }) {
   const [newItems, setNewItems] = useState([]);
@@ -15,16 +19,38 @@ export default function Cart({ items }) {
   }, [items]);
 
   return (
-    <div className="my-2">
-      {newItems.map((item) => {
-        const itemName = item.model.replace(/ /g, "_");
-        return (
-            <p key={itemName}>
-              {item.brand} {item.model}{" "}
-              {count[itemName] > 1 ? `* ${count[itemName]}` : null}
-            </p>
-        );
-      })}
+    <div className={`${styles.cart}`}>
+      <h2>Cart</h2>
+      <Table>
+        <tbody>
+          {newItems.map((item) => {
+            const itemName = item.model.replace(/ /g, "_");
+            return (
+              <tr key={itemName}>
+                <td>
+                <img 
+                  src="/img/icons/plus-lg.svg" 
+                  alt={`Add one ${item.brand} ${item.model}`}/>
+                  {count[itemName]}
+                <img 
+                  src="/img/icons/dash.svg" 
+                  alt={`Remove one ${item.brand} ${item.model}`}/>
+                </td>
+                <td>
+                {item.brand} {item.model}
+                </td>
+                <td>
+                  <img 
+                  src="/img/icons/trash3.svg" 
+                  alt={`Remove all ${item.brand} ${item.model}`}/>
+                </td>
+              </tr>
+            );
+          }
+          )}
+      </tbody>
+      </Table>
+      
     </div>
   );
 }
